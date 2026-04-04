b359ec208cc4807cf90f5c8637ec358d85666ca5
YusufErdemK <yusuferdemyek@gmail.com>
2026-04-04 10:16:12 +0300

    javascript: this file has a function that waits and sends you to another html
    Signed-off-by: YusufErdemK <yusuferdemyek@gmail.com>

---
+++ src/install-ui/gecikmemoruk.js
@@ -0,0 +1,14 @@
+function gecikmemoruk(url) {
+    document.getElementById('body').style.opacity = '0';
+    document.getElementById('body').style.transform = 'scale(0.9)';
+
+    // To Do: bu amk fonksiyonunu düzeltmeliyim
+    // if (!selectedDisk) {
+    //     alert('Lütfen bir disk seçin');
+    //     return;
+    // }
+
+    setTimeout(function () {
+        window.location.href = url;
+    }, 1000);
+}
